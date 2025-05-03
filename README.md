c8a2ae6cc144f1c45f535fd57e32732745cdc5b1
tropicbliss <tropicbliss@protonmail.com>
2025-05-03 15:21:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# freshkills
+
+A personal script to delete any gitignored files for faster backup. Extremely
+fast and dangerous, read the source code before running the command. Named after
+the [Fresh Kills Landfill](https://en.wikipedia.org/wiki/Fresh_Kills_Landfill)
+because it sounds cool and is a place where things go to die.
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::io::Write;
+
+use fnv::FnvHashSet;
+use ignore::WalkBuilder;
+use phf::phf_set;
+use walkdir::WalkDir;
+
+static IGNORED_FILES: phf::Set<&'static str> = phf_set!(".gitignore", ".env");
+const ROOT_PATH: &'static str = ".";
+
+fn main() {
+    let unignored_paths: FnvHashSet<_> = WalkBuilder::new(ROOT_PATH)
+        .require_git(false)
+        .build()
+        .filter_map(Result::ok)
+        .map(|entry| entry.path().as_os_str().to_owned())
+        .collect();
+    let stdout = std::io::stdout();
+    let mut lock = stdout.lock();
+    for entry in WalkDir::new(ROOT_PATH).contents_first(true) {
+        match entry {
+            Ok(entry) => {
+                if !unignored_paths.contains(entry.path().as_os_str())
+                    && !IGNORED_FILES.contains(&entry.file_name().to_string_lossy())
+                {
+                    println!("Deleting: {}", entry.path().display());
+                    if entry.path().is_file() {
+                        if fs::remove_file(entry.path()).is_err() {
+                            writeln!(lock, "Failed to delete file {}", entry.path().display())
+                                .unwrap();
+                        }
+                    } else if entry.path().is_dir() {
+                        if fs::remove_dir(entry.path()).is_err() {
+                            eprintln!("Failed to delete directory {}", entry.path().display());
+                        }
+                    }
+                }
+            }
+            Err(e) => writeln!(lock, "Error accessing entry: {}", e).unwrap(),
+        }
+    }
+}
